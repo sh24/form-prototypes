98971a6e42050530ffd65c459699eb3cf15a9ef8
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-06-10 15:02:55 +0100

    Add fith version: postcode validation (regex), form reordering

--- assets/js/modular-form.js
+++ assets/js/modular-form.js
@@ -1,3 +1,18 @@
+function checkPostcode(el) {
+  var output, colour;
+  var postcode = el.value.match(/^(SW8|SE11|SE1)(\s)?[a-zA-Z0-9]{3}$/i);
+  console.log("match postcode: ", postcode);
+  if (!!postcode) {
+    colour = 'rgb(149,233,149)';
+    output = 'Great, you live in the catchment area!<br/>';
+  } else {
+    colour = 'rgb(200,200,200)'
+    output = 'SH24 is currently only available to <strong>Lambeth</strong> and <strong>Southwark</strong> boroughs.';
+  }
+  document.getElementsByClassName('postcode-validation')[0].style.color = colour;
+  document.getElementById('postcodeValidationReport').innerHTML = output;
+}
+
 function inputAddress() {
   document.getElementById('field1').value = '1a House';
   document.getElementById('field2').value = '123 Street';
