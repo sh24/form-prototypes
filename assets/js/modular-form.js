c5119b5659d9512dbad2efbb8b0bfb9c5b3308b8
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-06-10 12:51:53 +0100

    Replace symptoms select with radio buttons. Add JS for example get addr

---
+++ assets/js/modular-form.js
@@ -0,0 +1,7 @@
+function inputAddress() {
+  document.getElementById('field1').value = '1a House';
+  document.getElementById('field2').value = '123 Street';
+  document.getElementById('field3').value = 'London';
+  document.getElementById('field4').value = 'London';
+  document.getElementById('field5').value = 'ABC 123';
+}
